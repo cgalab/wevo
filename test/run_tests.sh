660fb22f124f2cd6f23876a52b965ab105eeb551
Stefan de Lorenzo <slorenzo@cosy.sbg.ac.at>
2019-12-11 20:07:10 +0100

    Updated test files

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -9,12 +9,12 @@ results="results-$timestamp.csv"
 echo "size,runtime,oltime,evtime,coll-evs,dom-evs,edge-evs,inv-coll-evs,inv-dom-evs,inv-edge-evs" > "$results"
 mkdir "$inputs"
 
-for i in {8..15}; do
+for i in {6..11}; do
 	lower=$((2 ** $i))
 	upper=$((2 ** ($i + 1)))
-	python gen_inputs.py -n 50 -l "$lower" -u "$upper" -f "$inputs"
+	python gen_inputs.py -n 10 -l "$lower" -u "$upper" -f "$inputs"
 done
 
 for entry in "$inputs"/*.pnts; do
-	./wevo --input-file "$entry" --csv-file "$results"
+	./wevo --input-file "$entry" --csv-file "$results" --view
 done
